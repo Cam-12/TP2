9a1e95e1416e563797cfa1a085e641243f31afd5
Cam-12 <cameron.florence1@mail.com>
2024-11-08 14:35:58 +0100

    v1

---
+++ Intervalle.py
@@ -0,0 +1,7 @@
+nombre= int(input("Entrez un nombre décimal : "))
+x= nombre
+
+if x>=2 and x<3 or x>0 and x<=1 or x>=-10 and x<=-2:
+    print("x appartient à I")
+else:
+    print("x n'appartient pas à I")
