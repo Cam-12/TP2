93b3720c72e53c79222d84e264452d082aa51370
Cam-12 <cameron.florence1@mail.com>
2024-11-08 13:59:40 +0100

    v1

--- Fondue.py
+++ Fondue.py
@@ -1,7 +1,20 @@
-BASE=4
-fromage=800.0
-eau=2
-ail=2
-pain=400
+BASE= 4
+fromage= 800.0
+eau= 2
+ail= 2
+pain= 400
 
-nbConvives=int(input("Entrez le nombre de personne(s) conviées à la fondue : "))
+nbConvives= int(input("Entrez le nombre de personne(s) conviées à la fondue : "))
+print("Pour faire une fondue fribourgeoise pour", nbConvives, "personnes, il vous faut :")
+
+qfromage= fromage * nbConvives / BASE
+print("-", qfromage, "gr de fromage")
+
+qeau= eau * nbConvives / BASE
+print("-", qeau, "dl d'eau")
+
+qail= ail * nbConvives / BASE
+print("-", qail, "gousse(s) d'ail")
+
+qpain= pain * nbConvives / BASE
+print("-", qpain, "gr de pain")
